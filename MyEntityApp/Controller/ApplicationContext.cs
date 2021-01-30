2ef03a59c3fed8adfdc278bafffbdec8e7f67f46
Nikita Stashulenok <nstashylenok@mail.ru>
2021-01-30 14:06:33 +0300

    Добавьте файлы проекта.

---
+++ MyEntityApp/Controller/ApplicationContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MyEntityApp.Model;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyEntityApp.Controller
+{
+    public class ApplicationContext : DbContext
+    {
+        public DbSet<User> Users { get; set; }
+        public ApplicationContext()
+        {
+            Database.EnsureCreated();
+        }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=(localdb)\\mssqllocaldb;Database=myentityapp;Trusted_Connection=True;");
+        }
+    }
+}
---
+++ MyEntityApp/Program.cs
@@ -0,0 +1,35 @@
+﻿using MyEntityApp.Controller;
+using MyEntityApp.Model;
+using System;
+using System.Linq;
+
+namespace MyEntityApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (ApplicationContext db = new ApplicationContext())
+            {
+                //create 2 Users
+                var user1 = new User { Name = "Nikita", Age = 18 };
+                var user2 = new User { Name = "Alice", Age = 17 };
+
+                //add them to DataBase
+                db.Users.Add(user1);
+                db.Users.Add(user2);
+                db.SaveChanges();
+                Console.WriteLine("Save completed percfectly");
+
+                //get objects from the DB and output to the console 
+                var users = db.Users.ToList();
+                Console.WriteLine("list of objects: ");
+                foreach (var user in users)
+                {
+                    Console.WriteLine($"{user.Id}.{user.Name} -> {user.Age}");
+                }
+            }
+            Console.ReadKey();
+        }
+    }
+}
